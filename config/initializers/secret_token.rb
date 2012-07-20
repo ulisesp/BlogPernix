64b4ff3a7280b37cc2c7ed5fe19b1b9f9f951dea
Ulises <upacheco@pernix-solutions.com>
2012-07-20 15:58:01 -0600

    Creacion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogPernix::Application.config.secret_token = '7ab846298f61af3541e253640fda6f5fb7a3fafc410e25d63736b7f73b0707f7dd14d6b2ea2c8aaa7c0d0b987fd29bbacd91b2debb8a74be980758c0cea2fb33'
