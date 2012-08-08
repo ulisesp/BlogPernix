07ee4f025afd53f0b35252a4ea866315717da272
Ulises <upacheco@pernix-solutions.com>
2012-08-07 18:11:21 -0600

    Login pop up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 BlogPernix::Application.routes.draw do
+  resources :users
+
   get "home/index"
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ BlogPernix::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
